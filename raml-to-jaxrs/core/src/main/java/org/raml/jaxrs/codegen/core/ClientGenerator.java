44d839e4e35a7d90d9f7f466b7fe5193b2a60e0b
Michael Riedel <michael.riedel@hybris.com>
2015-02-19 15:47:50 +0100

    STA-1525: Adjust the implementation of ClientGenerator towards what we need. So far, generate simple client classes that have some action methods.

--- raml-to-jaxrs/core/src/main/java/org/raml/jaxrs/codegen/core/ClientGenerator.java
+++ raml-to-jaxrs/core/src/main/java/org/raml/jaxrs/codegen/core/ClientGenerator.java
@@ -15,62 +15,127 @@
  */
 package org.raml.jaxrs.codegen.core;
 
-import java.util.Collection;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
+import static org.apache.commons.lang.StringUtils.strip;
 
-import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.Collection;
+import java.util.Locale;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
 
 import org.raml.jaxrs.codegen.core.ext.GeneratorExtension;
 import org.raml.model.Action;
 import org.raml.model.MimeType;
+import org.raml.model.Raml;
+import org.raml.model.Resource;
 
+import com.sun.codemodel.JClass;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JDocComment;
+import com.sun.codemodel.JExpr;
 import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JPackage;
 import com.sun.codemodel.JType;
+import com.sun.codemodel.JVar;
 
 public class ClientGenerator extends AbstractGenerator {
 
-	protected void addResourceMethod(final JDefinedClass resourceInterface,
-			final String resourceInterfacePath, final Action action,
-			final MimeType bodyMimeType,
-			final boolean addBodyMimeTypeInMethodName,
-			final Collection<MimeType> uniqueResponseMimeTypes)
+	/**
+	 * {@inheritDoc} <br>
+	 * This implementation creates a resource client, rather than an interface!
+	 */
+	@Override
+	protected void createResourceInterface(Resource resource, Raml raml)
 			throws Exception {
-		final String methodName = Names.buildResourceMethodName(action,
-				addBodyMimeTypeInMethodName ? bodyMimeType : null);
-		final JType resourceMethodReturnType = getResourceMethodReturnType(
-				methodName, action, uniqueResponseMimeTypes.isEmpty(),
-				false, resourceInterface);
-		final JMethod method = context.createResourceMethod(resourceInterface,
-				methodName, resourceMethodReturnType);
-		context.addHttpMethodAnnotation(action.getType().toString(), method);
-		addParamAnnotation(resourceInterfacePath, action, method);
-		addConsumesAnnotation(bodyMimeType, method);
-		addProducesAnnotation(uniqueResponseMimeTypes, method);
-		final JDocComment javadoc = addBaseJavaDoc(action, method);
-		addParameters(action, bodyMimeType, method, javadoc);
+
+		final String resourceInterfaceName = Names
+				.buildResourceInterfaceName(resource);
+		final JPackage pkg = context.getCodeModel()._package(
+				context.getConfiguration().getBasePackageName() + ".builder");
+		final JDefinedClass resourceClient = pkg._class(resourceInterfaceName);
+		context.setCurrentResourceInterface(resourceClient);
+
+		final String path = strip(resource.getRelativeUri(), "/");
+		resourceClient.field(JMod.PUBLIC | JMod.STATIC | JMod.FINAL,
+				String.class, "PATH", JExpr.lit(path));
+
+		if (isNotBlank(resource.getDescription())) {
+			resourceClient.javadoc().add(resource.getDescription());
+		}
+
+		resourceClient.field(JMod.PRIVATE, Client.class, "_jaxRsClient");
+		resourceClient.field(JMod.PRIVATE, URI.class, "_baseUrl");
+
+		final JMethod createRequestTarget = resourceClient.method(
+				JMod.PROTECTED, WebTarget.class, "createRequestTarget");
+		createRequestTarget.body().directStatement(
+				"return _jaxRsClient.target(_baseUrl).path(PATH);");
+
+		addResourceMethods(resource, resourceClient, path);
+
 		/* call registered extensions */
 		for (GeneratorExtension e : extensions) {
-			e.onAddResourceMethod(method, action, bodyMimeType,
-					uniqueResponseMimeTypes);
+			e.onCreateResourceInterface(resourceClient, resource);
 		}
 	}
 
-	private void addParameters(final Action action,
-			final MimeType bodyMimeType, final JMethod method,
-			final JDocComment javadoc) throws Exception {
-		addPathParameters(action, method, javadoc);
-		addHeaderParameters(action, method, javadoc);
-		addQueryParameters(action, method, javadoc);
-		addBodyParameters(bodyMimeType, method, javadoc);
+	@Override
+	protected void addResourceMethod(
+			JDefinedClass resourceClient,
+			String resourceInterfacePath,
+			Action action,
+			MimeType requestMimeType,
+			boolean addBodyMimeTypeInMethodName,
+			Collection<MimeType> uniqueResponseMimeTypes) throws Exception {
+
+		final String actionName = action.getType().name().toLowerCase(Locale.ROOT);
+		final String methodName = Names.buildResourceMethodName(action, addBodyMimeTypeInMethodName ? requestMimeType : null);
+
+		if (uniqueResponseMimeTypes.isEmpty())
+		{
+			final JMethod resourceMethod = resourceClient.method(JMod.PUBLIC, Void.TYPE, methodName);
+
+			if(requestMimeType != null)
+			{
+				addBodyParameters(requestMimeType, resourceMethod, new JDocComment(context.getCodeModel()));
+				resourceMethod.body().directStatement("createRequestTarget().request()." + actionName + "(javax.ws.rs.client.Entity.entity(entity, \"" + requestMimeType.getType() + "\"));"); 
+			}
+			else
+			{
+				resourceMethod.body().directStatement("createRequestTarget().request()." + actionName + "();"); 
+			}
+		}
+		else
+		{
+			// obtain return type of resource
+			final JType methodReturnType = types.getResponseEntityClass(uniqueResponseMimeTypes.iterator().next());
+			final JMethod resourceMethod = resourceClient.method(JMod.PUBLIC, methodReturnType, methodName);
+			
+			String methodReturnTypeLiteral = methodReturnType.name() + ".class";
+			if(methodReturnType instanceof JClass)
+			{
+				final JClass methodReturnClass = (JClass) methodReturnType;
+				if(methodReturnClass.isParameterized())
+				{
+					methodReturnTypeLiteral = "new javax.ws.rs.core.GenericType<" + methodReturnType.name() + ">(){}";
+					
+				}
+				
+			}
+
+			if(requestMimeType != null)
+			{
+				addBodyParameters(requestMimeType, resourceMethod, new JDocComment(context.getCodeModel()));
+				resourceMethod.body().directStatement("return createRequestTarget().request()." + actionName + "(javax.ws.rs.client.Entity.entity(entity, \"" + requestMimeType.getType() + "\"), " + methodReturnTypeLiteral +");");
+			}
+			else
+			{
+				resourceMethod.body().directStatement("return createRequestTarget().request()." + actionName + "(" + methodReturnTypeLiteral +");");
+			}
+		}
 	}
 
-	private JType getResourceMethodReturnType(String methodName, Action action,
-			boolean empty, boolean async, JDefinedClass resourceInterface) {
-		if (empty&&context.getConfiguration().isEmptyResponseReturnVoid())
-        {
-            return types.getGeneratorType(Response.Status.class);
-        }
-		return context.ref(Response.class.getName());
-	}
 }
